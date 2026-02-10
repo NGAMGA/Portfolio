4377ee55c56f0baa89251409e81db816285a7ac9
Ashley NGAMGA <ashleyngamga@gamil.com>
2026-02-11 00:11:14 +0100

    Modifications du design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio chargé avec succès ");
+console.log("Portfolio chargé avec succès");
+
+const elements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+elements.forEach(el => observer.observe(el));
